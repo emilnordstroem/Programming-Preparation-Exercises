2a7b71db0d9f9c3691472f8f3dd47878c111b0fc
emilnordstroem <emilstoeve@outlook.com>
2025-08-27 15:05:41 +0200

    Implemented a group chat through TCP - using a server as a connection between multiple clients

---
+++ src/SocketProgramming/groupMessagePrototype/ClientTCP.java
@@ -0,0 +1,64 @@
+package SocketProgramming.groupMessagePrototype;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class ClientTCP {
+    private static DataOutputStream outToServer;
+    private static BufferedReader inFromServer;
+
+    public static void main(String[] args) {
+        try {
+            Socket clientSocket = new Socket("localhost", 10_000);
+            outToServer = new DataOutputStream(
+                    clientSocket.getOutputStream()
+            );
+            inFromServer = new BufferedReader(
+                    new InputStreamReader(
+                            clientSocket.getInputStream()
+                    )
+            );
+
+            Thread writeThread = new Thread(() -> writeToServer());
+            Thread readThread = new Thread(() -> readFromServer());
+
+            writeThread.start();
+            readThread.start();
+        } catch (UnknownHostException e){
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void writeToServer(){
+        Scanner messageScanner = new Scanner(System.in);
+        String messageToServer;
+        while (true) {
+            try {
+                messageToServer = messageScanner.nextLine();
+                outToServer.writeBytes(messageToServer + '\n');
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private static void readFromServer(){
+        String messageFromServer;
+        while (true) {
+            try {
+                messageFromServer = inFromServer.readLine();
+                System.out.println("Received: " + messageFromServer);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+}
